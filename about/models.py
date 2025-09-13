10076b15ff0dc0b0bc2b9329236857504c8a87c1
ManalAbduqawi <denever_2008@windowslive.com>
2025-09-13 14:47:21 +0100

    tidyup for tests

--- about/models.py
+++ about/models.py
@@ -1,17 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 
 # Create your models here.
 
 class About(models.Model):
+    """
+    Renders the most recent information on the website author
+    and allows user collaboration requests.
+
+    Displays an individual instance of :model:`about.About`.
+
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+        ``collaborate_form``
+            An instance of :form:`about.CollaborateForm`.
+    
+    **Template**
+    :template:`about/about.html`
+    """
     title = models.CharField()
     content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
     
 class CollaborateRequest(models.Model):
+    """
+    Stores a single collaboration request message
+    """
     name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,21 @@ from .forms import CommentForm
 
 # Create your views here.
 class PostList(generic.ListView):
+
+    """
+    Returns all published posts in :model:`blog.Post`
+    and displays them in a page of six posts. 
+    **Context**
+
+    ``queryset``
+        All published instances of :model:`blog.Post`
+    ``paginate_by``
+        Number of posts per page.
+        
+    **Template:**
+
+    :template:`blog/index.html`
+    """
     queryset = Post.objects.all().filter(status=1).order_by("-created_on")  
     template_name = "blog/index.html"
     paginate_by = 6
@@ -23,6 +38,12 @@ def post_detail(request, slug):
 
     ``post``
         An instance of :model:`blog.Post`.
+    ``comments``
+        All approved comments related to the post.
+    ``comment_count``
+        A count of approved comments related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
 
     **Template:**
 
@@ -63,6 +84,19 @@ def post_detail(request, slug):
 
 
 def comment_edit(request, slug, comment_id):
+     
+      """
+    Display an individual comment for edit.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    ``comment_form``
+       An instance of :form:`blog.CommentForm`
+      """
       
       if request.method == "POST":
           queryset = Post.objects.filter(status=1)
@@ -82,7 +116,14 @@ def comment_edit(request, slug, comment_id):
 
 def comment_delete(request, slug, comment_id):
     """
-    view to delete comment
+    Delete an individual comment.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
